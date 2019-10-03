658f628a58996cf5aebeed8bfa06882696dbcf18
Sam Forrester <SForrester@spartaglobal.com>
2019-10-03 11:06:59 +0100

    added a new file with using json.load() to take our thing that looks like a dictionary and make it into a dictionary

---
+++ json_load.py
@@ -0,0 +1,8 @@
+import json
+# Here we load/decode json
+
+with open('new_json_file.json', 'r') as jsonfile:
+    # convert to a dictionary
+    car_dictionary = json.load(jsonfile)
+    print(type(car_dictionary))
+    print(car_dictionary['brand'])
